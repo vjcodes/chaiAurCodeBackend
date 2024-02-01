951c5a438e973cd1cdcbf82d9f1984177d8fa264
vjcodes <vinayak.kris@gmail.com>
2024-02-01 14:35:04 +0530

    -edge case fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -28,8 +28,13 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for images, check for avatar
   //   console.log(req.files);
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
   //   console.log(avatarLocalPath);
+
+  let coverImageLocalPath;
+  if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+    coverImageLocalPath = req.files.coverImage[0].path
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
